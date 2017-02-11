b12def0c5de74ba39a37549d9464fef66f37c794
Ubuntu <ubuntu@ip-172-31-7-147.ec2.internal>
2017-02-11 04:20:42 +0000

    no memory of what changed since last commit. Good git usage!

--- install.sh
+++ install.sh
@@ -1,1 +1,6 @@
-pip install numpy scipy sckit-learn pillow h5py
+pip3 install numpy scipy scikit-learn pillow h5py
+pip3 install --upgrade --no-deps git+git://github.com/Theano/Theano.git
+#pip3 install keras
+export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0-cp35-cp35m-linux_x86_64.whl
+#export TF_BINARY_URL=https://storage.googleapis.com/tensorflow/linux/cpu/tensorflow-0.11.0-cp27-none-linux_x86_64.whl
+pip3 install --upgrade $TF_BINARY_URL
